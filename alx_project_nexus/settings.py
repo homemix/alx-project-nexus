370a0d8edf46cf271797b63b2bb285d7594304dd
homemix <kenmutati@gmail.com>
2025-07-24 10:48:52 +0300

    change project settings.

--- alx_project_nexus/settings.py
+++ alx_project_nexus/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.getenv('DJANGO_DEBUG')
 
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
